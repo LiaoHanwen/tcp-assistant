b689b271feeae1eaa2b1a1e0b8c531d7acd791d0
LiaoHanwen <login@liaohanwen.com>
2018-10-11 15:01:00 +0800

    update tcp_reader

--- tcp_reader.py
+++ tcp_reader.py
@@ -1,12 +1,31 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"  # IP for local host
 PORT = 9971         # Listen port
 
+
+# new thread
+class StartThread(threading.Thread):
+    '''New Thread'''
+
+    def __init__(self, clientSocket, addr):
+        threading.Thread.__init__(self)
+        self.clientSocket = clientSocket
+        self.addr = addr
+
+    def run(self):
+        print("accept tcp connection from: " + str(self.addr))
+        while(True):
+            message = self.clientSocket.recv()      # receive message
+            print(str(self.addr) + ": " + message)  # print
+
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-counter = 0  # counter for receive
-server.bind((HOST, PORT))  # bind port
+counter = 0                 # counter for receive
+server.bind((HOST, PORT))   # bind port
+server.listen(5)            # set max connections
 while(True):
-    counter += 1
-    data, addr = server.recvfrom(1024)
-    print(str(counter) + ": ", addr, data)
+    clientSocket, addr = server.accept()
+    thread = StartThread(clientSocket, addr)
+    thread.start()
